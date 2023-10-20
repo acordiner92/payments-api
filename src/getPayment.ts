d044414f6f268a91929b75a53d30575b8d8ab627
Andrew Cordiner <andrewcordiner92@gmail.com>
2023-10-20 15:15:36 +1100

    Implemented question 1

--- src/getPayment.ts
+++ src/getPayment.ts
@@ -1,5 +1,16 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { getPayment } from "./lib/payments";
+import { buildResponse } from "./lib/apigateway";
 
-export const handler = async (event:APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    throw Error("Implement me!")
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const id = event.pathParameters?.id;
+
+  if (!id) {
+    throw Error("TODO: handle this case");
+  }
+
+  const payment = await getPayment(id);
+  return buildResponse(200, { ...payment });
 };
